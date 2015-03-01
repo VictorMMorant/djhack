308fb7314d28c5517e9b3b31d32e4c4e8da11ab9
Mathieu Dutour <mathieu@dutour.me>
2015-03-01 06:42:25 +0000

    swipe fix + background fix

--- client/controlers/client.js
+++ client/controlers/client.js
@@ -1,6 +1,9 @@
 Template.client.helpers({
 	songs : function() {
-		return Songs.find({ partyId: Session.get("partyId"), archived: false, alreadyPlayed: false }, {sort:[["votesCount", "desc"]]});
+		return Songs.find({ partyId: Session.get("partyId"), archived: false, alreadyPlayed: false }, {sort:[["votesCount", "desc"]]}).map(function(song) {
+          song.swipable = !song.isPlaying && !Votes.findOne({userId: Session.get('userId'), songId: song._id});
+          return song;
+        });
 	}
 });
 
@@ -13,38 +16,36 @@ Template.client.events({
 });
 
 Template.song.events({
-	"click .upvoted" : function(e) {
+	"click .upvoted" : function(e, t) {
 		e.preventDefault();		
 		var userId = Session.get("userId");
 		var songId = e.currentTarget.id;
 		Meteor.call("vote", userId, songId, 1, function(error, result){
 			if(error)
-				console.log("Can't upvote the song", error);
-            else
-              $(this.find(".song")).removeClass("swiped");
+		      console.log("Can't upvote the song", error);
+            $(t.find(".song")).removeClass("swiped");
 		});
 		
 	},
-	"click .downvoted" : function(e) {
+	"click .downvoted" : function(e, t) {
 		e.preventDefault();
 		var userId = Session.get("userId");
 		var songId = e.currentTarget.id;
 		Meteor.call("vote", userId, songId, -1, function(error, result){
 			if(error)
-				console.log("Can't downvote the song", error);
-            else
-              $(this.find(".song")).removeClass("swiped");
+		      console.log("Can't downvote the song", error);
+            $(t.find(".song")).removeClass("swiped");
 			
 		});
 	}
 });
 
 Template.song.gestures({
-  'swipeleft .song': function (e) {
+  'swipeleft .song.swipable': function (e) {
     e.preventDefault();
     $(this.find(".song")).addClass("swiped");
   },
-  'swiperight .song': function (e) {
+  'swiperight .song.swipable': function (e) {
     e.preventDefault();
     $(this.find(".song")).removeClass("swiped");
   }
