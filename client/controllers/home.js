ffe37f1197547e9a5951e5addbbf8b8f4a59c115
Mathieu Dutour <mathieu@dutour.me>
2015-03-01 10:12:42 +0000

    bs + team

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -5,6 +5,45 @@ UI.body.helpers({
   }
 });
 
+Template.home.helpers({
+  members: function() {
+    return [
+      {
+        name: 'Mathieu Dutour',
+        imageBlack:  "images/mathieu_black.jpg",
+        image:  "images/mathieu.jpg",
+        facebook: "https://www.facebook.com/mathieu.dutour",
+        twitter: "https://twitter.com/MathieuDutour",
+        linkedin: "https://uk.linkedin.com/in/mathieudutour"
+      },
+      {
+        name: 'Miguel Martínez',
+        imageBlack:  "images/miguel_black.jpg",
+        image:  "images/miguel.jpg",
+        facebook: "",
+        twitter: "",
+        linkedin: ""
+      },
+      {
+        name: 'Victor Martínez',
+        imageBlack:  "images/victor_black.jpg",
+        image:  "images/victor.jpg",
+        facebook: "",
+        twitter: "",
+        linkedin: ""
+      },
+      {
+        name: 'Miao Sun',
+        imageBlack:  "images/miao_black.jpg",
+        image:  "images/miao.jpg",
+        facebook: "",
+        twitter: "",
+        linkedin: ""
+      }
+    ];
+  }
+});
+
 Template.home.events({
 	"click .create" : function(e) {
 		e.preventDefault();
