7319187e30750d80593ec0c355f4bf096912145e
Miguel Martinez <miguel.martinez.inf@gmail.com>
2015-05-29 12:37:17 +0200

    Removed console.log printing id

--- client/controllers/routes.js
+++ client/controllers/routes.js
@@ -1,11 +1,8 @@
-
-
 Session.set("page", "home");
 Session.set("userId",  Session.get("userId") || new Mongo.ObjectID().toHexString());
 
 UI.body.rendered = function() {
   var id = location.hash.substring(2);
-  console.log(id);
   if (id.length === 0) {
     location.hash = "#/";
   } else {
